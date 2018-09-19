50409d7ae350b1ee649431b49bec4991a718fb57
Jose Riaño <jriano@avanxo.com>
2018-09-19 16:30:41 -0500

    Use of Action

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,17 +9,31 @@ namespace ConsoleApp1
             MyClass myClass = new MyClass();
 
             // Pass a regular method as a callback
-            myClass.LongRunningMethod(printProgress);
+            myClass.LongRunningMethod1(printProgress);
 
             Console.WriteLine("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
 
             // Pass a Lambda expression as a callboack
-            myClass.LongRunningMethod(progress => 
+            myClass.LongRunningMethod1(progress => 
             {
                 if (progress % 100 == 0) // Print only multiples of 100
                     Console.WriteLine(progress);
             });
 
+            Console.WriteLine("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+            Console.WriteLine("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+
+            // Using System Action
+            myClass.LongRunningMethod2(printProgress);
+
+            Console.WriteLine("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+
+            // Lambda expressions are equivalent to a delegate, so this works exactly the same as before.
+            myClass.LongRunningMethod2((progress) =>
+            {
+                if (progress % 100 == 0) // Print only multiples of 100
+                    Console.WriteLine(progress);
+            });
 
             Console.ReadLine();
         }
@@ -36,7 +50,17 @@ namespace ConsoleApp1
     class MyClass
     {
         public delegate void Callback(int processedLines);
-        public void LongRunningMethod(Callback callback)
+        public void LongRunningMethod1(Callback callback)
+        {
+            for (int i = 0; i < 1001; i++)
+            {
+                callback(i);
+            }
+        }
+
+        // An alternative using the System's Action. 
+        // An action is simply a delegate with no return type.
+        public void LongRunningMethod2(Action<int> callback)
         {
             for (int i = 0; i < 1001; i++)
             {
