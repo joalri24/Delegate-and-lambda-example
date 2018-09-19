f27dcec74ac6d6abe1d11e0d43f03875277f93b6
Jose Riaño <jriano@avanxo.com>
2018-09-19 15:53:44 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            // Pass a regular method as a callback
+            myClass.LongRunningMethod(printProgress);
+
+            Console.WriteLine("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+
+            // Pass a Lambda expression as a callboack
+            myClass.LongRunningMethod(progress => 
+            {
+                if (progress % 100 == 0) // Print only multiples of 15
+                    Console.WriteLine(progress);
+            });
+
+
+            Console.ReadLine();
+        }
+
+        private static void printProgress (int progress)
+        {
+            if (progress % 24 == 0) // Print only multiples of 24
+                Console.WriteLine(progress);
+        }
+
+        
+    }
+
+    class MyClass
+    {
+        public delegate void Callback(int processedLines);
+        public void LongRunningMethod(Callback callback)
+        {
+            for (int i = 0; i < 1001; i++)
+            {
+                callback(i);
+            }
+        }
+
+    }
+}
